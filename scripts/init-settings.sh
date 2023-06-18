7aa81796b87e78e3203fd0c86a4c9a8581875a79
Cong Bao <bao_cong@outlook.com>
2023-06-18 19:03:15 +0800

    openwrt-23.05

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -10,7 +10,7 @@
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
 # Disable IPV6 ula prefix
-sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+# sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
